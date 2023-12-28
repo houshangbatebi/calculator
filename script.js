c19d5e78607819498660786cd7f9ec72a21cba6b
houshangbatebi <109485943+houshangbatebi@users.noreply.github.com>
2023-12-28 22:34:58 +0330

    Update the screen when numeric button is clicked

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-// store the current operands and operator
-let firstNumber
-let operator
-let secondNumber
+// Store the current operands and operator
+let firstNumber = 0
+let operator = undefined
+let secondNumber = undefined
+
+let currentNumber = 0
+let digitCounter = 0
+const screen = document.querySelector('#screen')
+
+// Digit button click handling
+const digitButtons = document.querySelectorAll('.digit')
+digitButtons.forEach((digitBtn) => {
+    digitBtn.addEventListener('click', (event) => {
+        if(digitCounter == 0 && event.target.textContent != 0) {
+            screen.textContent = event.target.textContent
+            digitCounter++
+        }
+        else if(digitCounter >0 && digitCounter < 10){
+            screen.textContent += event.target.textContent
+            digitCounter++
+        }
+        currentNumber = screen.textContent
+    })
+})
 
 // Do calculation
 function operate(firstNum, secondNum, operator) {
