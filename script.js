7c427e930a9d59e0b4ca5ef411b4331b17bb2533
houshangbatebi <109485943+houshangbatebi@users.noreply.github.com>
2023-12-28 17:55:53 +0330

    Create a function to do a single calculation

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// store the current operands and operator
+let firstNumber
+let operator
+let secondNumber
+
+// Do calculation
+function operate(firstNum, secondNum, operator) {
+    if (operator == '+') {
+        return add(firstNum, secondNum)
+    }
+    if (operator == '-') {
+        return subtract(firstNum, secondNum)
+    }
+    if (operator = '*') {
+        return multiply(firstNum, secondNum)
+    }
+    if (operator == '/') {
+        return divide(firstNum, secondNum)
+    }
+}
+
+// Basic math functions
 function add(a, b) {
     return a + b
 }
